55ec1dea4380dcbbca322cecaba592b803cefed5
Tommaso <tomas97186@gmail.com>
2021-06-02 15:56:08 +0200

    Modifica evaluation

--- fitness_functions.py
+++ fitness_functions.py
@@ -1,10 +1,21 @@
 import numpy as np
 
 def track_center_fitness(res):
-    return np.sum(np.abs(res['trackPos']))
+    x1 = 0 if res['distRaced']>=res['laplength'] else  res['laplength'] - res['distRaced']
+    return np.sum(np.abs(res['trackPos'])) + x1
+
+def fitness_time(res):
+    x1 = 0 if res['distRaced']>=res['laplength'] else res['laplength'] - res['distRaced']
+    return res['lapTime'] + x1
+
+def fitness_speed(res):
+    x1 = 0 if res['distRaced']>=res['laplength'] else  res['laplength'] - res['distRaced']
+    x2 = res['lapTime'] if res['lapTime'] > 50 else 1000
+    x3 = res['laplength'] if res['laplength'] !=0 else 10
+    return (x2 / x3) + x1
 
 def fitness_1(res):
-    x1 = res['lapTime'] if res['lapTime'] > 50 else 1000
-    x2 = res['laplength'] if res['laplength'] !=0 else 10
-    x3 = 0 if res['distRaced']>=res['laplength'] else 1000
-    return (x1 / x2) + np.mean(np.abs(res['trackPos'])) + x3
+    x1 = fitness_speed(res)
+    x2 = np.sum(np.abs(res['trackPos']))
+    return x1 + x2
+
